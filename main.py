24e495bc4026ca06a1cce8ac2e8c5dcb478d1d01
AnshumanMohanty-2001 <anshu.mohanty.123102977@gmail.com>
2023-11-27 23:01:47 -0800

    Added analysis code

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from src.get_data import *
 from src.clean_data import *
+from src.run_analysis import *
 from src.visualize_results import *
 
 if __name__ == '__main__':
-    get_data()
+    # get_data()
+    #
+    # clean_data()
 
-    clean_data()
+    run_analysis()
 
-    visualize_results()
+    # visualize_results()
