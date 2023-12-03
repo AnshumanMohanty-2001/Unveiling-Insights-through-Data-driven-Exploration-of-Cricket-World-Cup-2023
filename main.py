9b28ff7c7221004a070ad808b5e206ca9e81177e
AnshumanMohanty-2001 <anshu.mohanty.123102977@gmail.com>
2023-12-03 10:15:01 -0800

    Updated main.py

--- main.py
+++ main.py
@@ -1,13 +1,30 @@
+import argparse
 from src.get_data import *
 from src.clean_data import *
 from src.run_analysis import *
 from src.visualize_results import *
 
 if __name__ == '__main__':
-    get_data()
 
-    clean_data()
+    parser = argparse.ArgumentParser(description='Process data')
 
-    run_analysis()
+    # Define command-line arguments
+    parser.add_argument('-clean', action='store_true', help='Clean data')
+    parser.add_argument('-get', action='store_true', help='Get data')
+    parser.add_argument('-analyze', action='store_true', help='Analyze data')
+    parser.add_argument('-visualize', action='store_true', help='Visualize data')
 
-    visualize_results()
+    # Parse the command-line arguments
+    args = parser.parse_args()
+
+    if args.get:
+        get_data()
+
+    elif args.clean:
+        clean_data()
+
+    elif args.analyze:
+        run_analysis()
+
+    elif args.visualize:
+        visualize_results()
